ec45d343fe296b73e30662e1c1eb4f4ad81e048a
GiYoung <rlduddl5519@naver.com>
2026-02-11 17:35:20 +0900

    2026-02-11, 파이썬 기초

---
+++ index.py
@@ -0,0 +1,13 @@
+ipadd="192.168.111.100"
+print(ipadd[5])
+print("192.168.111.100"[5])
+print(ipadd[-1])
+print(ipadd[-2])
+print(ipadd[-3])
+print(ipadd[0:3])
+print(ipadd[-3:-1])
+
+
+print(len(ipadd))
+a=len(ipadd)
+print(a)
